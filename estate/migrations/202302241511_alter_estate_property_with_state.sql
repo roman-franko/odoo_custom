ee6983cbb9cfb9ac5dbb9e78e3590cc92ef43548
Roman Franko <roman.franko00@gmail.com>
2023-02-24 15:24:51 +0200

    added state column to estate_property table

---
+++ estate/migrations/202302241511_alter_estate_property_with_state.sql
@@ -0,0 +1,15 @@
+-- Add a state field to the estate.property model. 
+-- Five values are possible: New, Offer Received, Offer Accepted, Sold and Canceled. 
+
+ALTER TABLE estate_property ADD state character varying DEF;
+
+ALTER TABLE estate_property 
+   ADD CONSTRAINT check_state_types 
+   CHECK (state = 'New' 
+       OR state = 'Offer Received'
+       OR state = 'Offer Accepted'
+       OR state = 'Sold'
+       OR state = 'Canceled');
+
+ALTER TABLE estate_property 
+    ALTER COLUMN state SET DEFAULT 'New';
