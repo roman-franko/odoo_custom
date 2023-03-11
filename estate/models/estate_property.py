5887f2bb7883ae80ad73e9fe1e755a29336ca4f4
Roman Franko <roman.franko00@gmail.com>
2023-03-11 22:15:37 +0200

    Set values for garden area and orientation

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -44,3 +44,12 @@ class EstateProperty(models.Model):
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'North'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = ''
